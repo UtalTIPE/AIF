680c91ad9ae7442a242eb696d94a40e8864e41a8
UtalTIPE <Digitality@outlook.fr>
2025-02-28 12:52:47 +0100

    add modifs

--- developpement/MNIST/mnist_webapp.py
+++ developpement/MNIST/mnist_webapp.py
@@ -3,14 +3,17 @@ from PIL import Image
 import requests
 import io
 
-
+import numpy as np
 def recognize_digit(image):
     # Convert to PIL Image necessary if using the API method
+    image = image['composite']
     image = Image.fromarray(image.astype('uint8'))
-    img_binary = ...
-    ...
-    ...
-    return ....
+    image = image.convert('L')
+    img_binary = io.BytesIO()
+    image.save(img_binary, format="PNG")
+    response = requests.post("http://127.0.0.1:5000/predict", data=img_binary.getvalue())
+    predict = response.json()["prediction"]
+    return predict
 
 if __name__=='__main__':
 
@@ -19,4 +22,4 @@ if __name__=='__main__':
                 outputs='label',
                 live=True,
                 description="Draw a number on the sketchpad to see the model's prediction.",
-                ).launch(debug=True, share=True);
+                ).launch(debug=True, share=True)
